198363c3530b88fafbb232200c6fb03ec7ea7ac8
Owen Conoly <otconoly@hotmail.com>
2025-03-18 02:01:16 -0400

    make build.sh file because coq_makefile is not cooperating

---
+++ build.sh
@@ -0,0 +1,4 @@
+coqc -Q bedrock2/bedrock2/src/bedrock2 bedrock2 -Q bedrock2/deps/coqutil/src/coqutil coqutil -Q equiv equiv equiv/EquivDefinitions.v
+coqc -Q bedrock2/bedrock2/src/bedrock2 bedrock2 -Q bedrock2/deps/coqutil/src/coqutil coqutil -Q equiv equiv equiv/EquivProof.v
+coqc -Q bedrock2/bedrock2/src/bedrock2 bedrock2 -Q bedrock2/deps/coqutil/src/coqutil coqutil -Q equiv equiv equiv/EquivTheorem.v
+coqc -Q bedrock2/bedrock2/src/bedrock2 bedrock2 -Q bedrock2/deps/coqutil/src/coqutil coqutil -Q equiv equiv equiv/OtherTheorems.v
